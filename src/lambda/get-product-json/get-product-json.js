2cb3d30df5bd65a0dd7089d2644e6a3552891af0
Romain Tello <44443864+Moonpearl@users.noreply.github.com>
2025-04-24 18:32:23 +0200

    Release 2.1 (fix Snipcart integration) (#18)
    * Update dependencies
    
    * Update Snipcart product JSON
    
    * Update package.json

--- src/lambda/get-product-json/get-product-json.js
+++ src/lambda/get-product-json/get-product-json.js
@@ -1,18 +1,17 @@
-const { GraphQLClient } = require('graphql-request');
+const { GraphQLClient } = require("graphql-request");
 
-const handler = async (event) => {
+const handler = async (event, context) => {
+  console.log({ event, context });
+
   const { GRAPHCMS_ENDPOINT, GRAPHCMS_MUTATION_TOKEN } = process.env;
 
   try {
     const { slug } = event.queryStringParameters;
-    const graphcms = new GraphQLClient(
-      GRAPHCMS_ENDPOINT,
-      {
-        headers: {
-          authorization: `Bearer ${GRAPHCMS_MUTATION_TOKEN}`,
-        }
-      }
-    );
+    const graphcms = new GraphQLClient(GRAPHCMS_ENDPOINT, {
+      headers: {
+        authorization: `Bearer ${GRAPHCMS_MUTATION_TOKEN}`,
+      },
+    });
 
     const { product } = await graphcms.request(
       `query ProductJsonQuery(
@@ -32,47 +31,69 @@ const handler = async (event) => {
         }
       }`,
       {
-        slug
+        slug,
       }
     );
 
     const { productVariants } = product;
 
+    const url = new URL(event.path, process.env.SITE_URL);
+    Object.entries(event.queryStringParameters).forEach(([key, value]) =>
+      url.searchParams.append(key, value)
+    );
+
     return {
       statusCode: 200,
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
       body: JSON.stringify({
-        url: `/.netlify/functions/get-product-json?slug=${slug}`,
         id: product.ebpId,
         name: product.name,
+        url: url.toString(),
         price: product.price,
         image: product.imageUrl,
         stock: product.lastReportedStock,
         description: product.description,
-        inventoryManagementMethod: productVariants.length === 0 ? 'Single' : 'Variant',
-        allowOutOfStockPurchases: productVariants.length === 0 ? false : undefined,
-        variants: productVariants.length === 0 ? undefined : [
-          {
-            variation: productVariants.map(variant => ({ name: 'Variante', option: variant.name })),
-            stock: product.lastReportedStock,
-            allowOutOfStockPurchases: true,
-          }
-        ],
-        customFields: productVariants.length === 0 ? undefined : [
-          {
-            name: 'Variante',
-            options: productVariants.map(
-              variant => `${variant.name}${variant.priceModifier !== 0 ? `[${variant.priceModifier}]` : ''}`
-            ).join('|'),
-            type: 'dropdown'
-          }
-        ]
+        inventoryManagementMethod:
+          productVariants.length === 0 ? "Single" : "Variant",
+        allowOutOfStockPurchases:
+          productVariants.length === 0 ? false : undefined,
+        variants:
+          productVariants.length === 0
+            ? undefined
+            : [
+                {
+                  variation: productVariants.map((variant) => ({
+                    name: "Variante",
+                    option: variant.name,
+                  })),
+                  stock: product.lastReportedStock,
+                  allowOutOfStockPurchases: true,
+                },
+              ],
+        customFields:
+          productVariants.length === 0
+            ? []
+            : [
+                {
+                  name: "Variante",
+                  options: productVariants
+                    .map(
+                      (variant) =>
+                        `${variant.name}${
+                          variant.priceModifier !== 0
+                            ? `[${variant.priceModifier}]`
+                            : ""
+                        }`
+                    )
+                    .join("|"),
+                  type: "dropdown",
+                },
+              ],
       }),
     };
-  }
-  catch (error) {
+  } catch (error) {
     return { statusCode: 500, body: error.toString() };
   }
 };
